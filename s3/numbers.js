8e97045320361cf2978f568b37bfaf7fe13e744e
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-11-22 22:17:57 -0600

    operations

---
+++ s3/numbers.js
@@ -0,0 +1,24 @@
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function substract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiplies(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+
+module.exports = {
+  add: add,
+  substract: substract,
+  multiplies: multiplies,
+  divide: divide
+};
---
+++ s3/operations.js
@@ -0,0 +1,49 @@
+var prompt = require('prompt');
+var log = require('./logWrapper.js');
+var numbers = require('./numbers.js');
+
+
+log("What operation do you want to do?");
+log("1 - numeric");
+log("2 - string");
+log("3 - date");
+
+prompt.start();
+
+prompt.get(['opType'], function(err, operation) {
+  log('You selected: ' + operation.opType);
+
+  if (operation.opType == 1) {
+
+
+    log('Now write the operation and the numbers');
+    log('1 - add');
+    log('2 - substract');
+    log('3 - multiplies');
+    log('4 - divide');
+
+    prompt.get(['op', 'num1', 'num2'], function(err, nums) {
+      if (nums.op == 1) {
+        log(numbers.add(nums.num1, nums.num2));
+      }
+      if (nums.op == 2) {
+        log(numbers.substract(nums.num1, nums.num2));
+      }
+      if (nums.op == 3) {
+        log(numbers.multiplies(nums.num1, nums.num2));
+      }
+      if (nums.op == 4) {
+        log(numbers.divide(nums.num1, nums.num2));
+      }
+    });
+  }
+
+
+
+  /*if (result.opType === 1) {
+
+  }
+
+  console.log(numbers.add(n1, n2));*/
+
+});
